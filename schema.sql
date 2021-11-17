2c968748dc276c68b15b2594ec054320cac1908b
Carl Sverre <carl@singlestore.com>
2021-11-17 11:37:53 -0800

    slight more to the demo

--- schema.sql
+++ schema.sql
@@ -16,3 +16,7 @@ insert into posts values (2, "read the damn docs!");
 insert into posts values (3, "I am a huge fan of pineapples");
 
 select body, sentiment(body) from posts;
+
+insert into posts select * from posts;
+
+select count(*) from posts;
